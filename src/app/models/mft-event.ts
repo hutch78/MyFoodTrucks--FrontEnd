e75b3ca716dc88c531940bbd0182e01a77083866
Jeremy Hutchcraft <hutch78@me.com>
2017-02-21 00:12:19 -0500

    <model> create MFTEvent model

---
+++ src/app/models/mft-event.ts
@@ -0,0 +1,13 @@
+export class MFTEvent {
+    id: number;
+    title: string;
+    description: string;
+    start_at: string;
+    end_at: string;
+    address_1: string;
+    city: string;
+    state: string;
+    zip: string;
+    created_at: string;
+    updated_at: string;
+}
